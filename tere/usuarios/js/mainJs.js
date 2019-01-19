c821366b153f08a2e077b433d42d67fab8b6c821
LtTere <tere_valdayo@hotmail.com>
2019-01-19 14:05:47 +0100

    ajax y script

--- tere/usuarios/js/mainJs.js
+++ tere/usuarios/js/mainJs.js
@@ -4,8 +4,9 @@ const myApi= 'https://randomuser.me/api/'
 async function llamadaApi(){
     let random = await fetch(myApi)
     random = await random.json()
-    console.log(random) 
+   console.log(random) 
     
 }
 
 llamadaApi()
+
