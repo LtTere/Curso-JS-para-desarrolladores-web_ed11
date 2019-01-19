e64bf32c770be09db1b95723380f9dd155027929
LtTere <tere_valdayo@hotmail.com>
2019-01-19 14:43:29 +0100

    manejo de datos

--- tere/usuarios/js/mainJs.js
+++ tere/usuarios/js/mainJs.js
@@ -4,9 +4,19 @@ const myApi= 'https://randomuser.me/api/'
 async function llamadaApi(){
     let random = await fetch(myApi)
     random = await random.json()
-   console.log(random) 
+    const baseUser = random.results[0]
+    represent(baseUser)
     
 }
 
 llamadaApi()
+
+function represent(baseUser){
+    
+    const myCanvas = document.getElementById('pintame')
+    console.log(baseUser)
+
+    myCanvas.innerHTML += `${baseUser.name.first} ${baseUser.name.last}`
+    myCanvas.innerHTML += `${baseUser.email}`
+}
 
