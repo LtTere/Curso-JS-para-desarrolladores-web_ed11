6a6a81e28ce44b840cc307e0bd234835a1935f1d
LtTere <tere_valdayo@hotmail.com>
2019-01-19 13:45:11 +0100

    app create

---
+++ tere/usuarios/js/mainJs.js
@@ -0,0 +1,11 @@
+
+const myApi= 'https://randomuser.me/api/'
+
+async function llamadaApi(){
+    let random = await fetch(myApi)
+    random = await random.json()
+    console.log(random) 
+    
+}
+
+llamadaApi()
